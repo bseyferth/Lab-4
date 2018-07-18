24c8fab6e60e695bd60e74938c9e69443c510d3c
Brian <bseyferth2@gmail.com>
2018-07-18 15:56:56 -0400

    Added notes and Goodbye Statement

--- src/squaresAndCubes/SquaresAndCubes.java
+++ src/squaresAndCubes/SquaresAndCubes.java
@@ -7,31 +7,43 @@ public class SquaresAndCubes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//open Scanner
 		Scanner scnr = new Scanner(System.in);
 		
+		//Declare variables
 		int i;
 		int userInput;
 		char restart;
 		
-		
+		//Initial do loop is for the continuation question.
 		do {
+			//this do loop verifies your number is positive.
 			do {
 			System.out.println("Please enter a positive integer:");
 			userInput = scnr.nextInt();
 			}
 			while (userInput < 1);
 			
-			System.out.println("Number\tSquare\tCube");
-			System.out.println("======\t======\t======");
+			//prints out the initial formatting titles and lines
 			
+			System.out.println("Number\tSquare\tCube");
+			System.out.println("=======\t=======\t=======");
+			
+			
+			//prints out the response based on user input in correct spacing
 			for (i = 1; i <= userInput; i++) {
 				System.out.println(i + "\t" + (i *i) + "\t" + ((i * i) * i));	
 			}
 			
+			//collects restart response from user and reacts
 			System.out.println("Would you like to do it again? (y to continue)");
 			restart = scnr.next().charAt(0);
 		}
 			while (restart == 'y');
+		
+		//alerts user program ended
+		System.out.println("Goodbye!");
+		scnr.close();
 	}
 
 }
